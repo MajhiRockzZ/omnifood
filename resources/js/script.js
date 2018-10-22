5e7da0d871a4a2c78bf7a4bb89fe13cd32ab2b6e
Sumesh Majhi <sumesh.majhi.5@gmail.com>
2018-10-22 19:46:50 +0530

    Update script.js file

--- resources/js/script.js
+++ resources/js/script.js
@@ -86,7 +86,7 @@ $(document).ready(function () {
      */
     $('.js--nav-icon').click(function () {
         var nav = $('.js--main-nav');
-        nav.slideToggle(200)
+        nav.slideToggle(200);
 
         // var icon = $('js--nav-icon i');
 
